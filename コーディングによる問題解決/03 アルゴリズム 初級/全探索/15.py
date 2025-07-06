ec36610870aae42aa03c573d2c90f32ae5357a75
kaneda05 <kanemasa1541km@gmail.com>
2025-07-06 23:15:03 +0900

    solve

---
+++ コーディングによる問題解決/03 アルゴリズム 初級/全探索/15.py
@@ -0,0 +1,9 @@
+N, M = map(int,input().split())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+cnt = 0
+for i in range(N):
+    for j in range(M):
+        if A[i] > B[j]:
+            cnt += 1
+print(cnt)
